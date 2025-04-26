c9705f2e465a016161ddd7032660411a9514027d
VaninaLuna <vani.lunaa@gmail.com>
2025-04-26 20:29:42 -0300

    perfil de usuario

---
+++ proyecto/Controllers/AccountController.cs
@@ -0,0 +1,50 @@
+﻿using proyecto.Bussines;
+using proyecto.DTOs;
+using System.Linq;
+using System.Web.Mvc;
+using DNF.Security.Bussines;
+
+namespace proyecto.Controllers
+{
+    [Authenticated]
+    public class AccountController : Controller
+    {
+        public ActionResult Profile()
+        {
+            User currentUser = Current.User;
+            var currentProfile = new ProfileDTO
+            {
+                UserName = currentUser.FullName,
+                UserEmail = currentUser.Email,
+                UserRol = "Administrador"
+            };
+
+            if (currentUser.Profiles.Any(p => p.Id == 2))
+            {
+                var currentAuditor = Auditor.Dao.GetByUser(currentUser.Id);
+                var auditorStatus = AuditorStatus.Dao.Get(currentAuditor.AuditorStatus.Id);
+
+                currentProfile.FileNumber = currentAuditor.FileNumber;
+                currentProfile.StartDate = currentAuditor.StartDate;
+                currentProfile.StartDateString = currentAuditor.StartDate.ToString("yyyy-MM-dd");
+                currentProfile.Status = auditorStatus.Name;
+
+                currentProfile.UserRol = "Auditor";
+            }
+            else if (currentUser.Profiles.Any(p => p.Id == 4))
+            {
+                var currentResponsible = Responsible.Dao.GetByUser(currentUser.Id);
+                var responsibleStatus = AuditorStatus.Dao.Get(currentResponsible.ResponsibleStatus.Id);
+
+                currentProfile.FileNumber = currentResponsible.FileNumber;
+                currentProfile.StartDate = currentResponsible.StartDate;
+                currentProfile.StartDateString = currentResponsible.StartDate.ToString("yyyy-MM-dd");
+                currentProfile.Status = responsibleStatus.Name;
+
+                currentProfile.UserRol = "Responsable";
+            }
+
+            return View(currentProfile);
+        }        
+    }
+}
---
+++ proyecto/DTOs/ProfileDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace proyecto.DTOs
+{
+    public class ProfileDTO
+    {
+        public int Id { get; set; }
+        public string FileNumber { get; set; }
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime StartDate { get; set; }
+        public string StartDateString { get; set; }
+        public string Status { get; set; }
+        public string UserRol { get; set; }
+        public string UserName { get; set; }
+        public string UserEmail { get; set; }
+    }
+}
